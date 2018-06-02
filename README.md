5bc9a118fe7779a00038f9d68946bd6b33c03a19
mukeshrathore <mukesh.rathore86@gmail.com>
2018-06-02 04:03:30 -0400

    application in progress using image
    next step to include xls file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # file-upload-with-express-multer-typescript
-<!-- based on https://scotch.io/tutorials/express-file-uploads-with-multer article -->
+<!-- below repo is based on https://scotch.io/tutorials/express-file-uploads-with-multer article and repo is located at https://github.com/chybie/file-upload-express -->
+step1: npm install express cors multer lokijs del --save
+step2: npm install typescript @types/express @types/multer @types/lokijs @types/del --save-dev
+step3: Add a typescript configuration file. i.e tsconfig.json
+step4: add prestart and start scripts in package.json
+Note: When we run "npm start", it will trigger prestart script first. The command "tsc" will read the tsconfig.json file and compile all typescript files to javascript in "dist" folder.
+Then, we will run the compiled index file "dist/index.js".
+step5: create Express server by creating index.ts file. We allow Cross-Origin Resource Sharing (CORS), set the connection port to 3000, and start the server.
---
+++ index.ts
@@ -0,0 +1,37 @@
+import * as express from 'express'
+import * as multer from 'multer'
+import * as cors from 'cors'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as Loki from 'lokijs'
+import { loadCollection, cleanFolder } from './utils';
+
+// setup
+const DB_NAME = 'db.json';
+const COLLECTION_NAME = 'images';
+const UPLOAD_PATH = 'uploads';
+const upload = multer({ dest: `${UPLOAD_PATH}/` }); // multer configuration
+const db = new Loki(`${UPLOAD_PATH}/${DB_NAME}`, { persistenceMethod: 'fs' });
+
+// app
+const app = express();
+app.use(cors());
+
+app.listen(3000, function () {
+    console.log('listening on port 3000!');
+});
+
+// optional: clean all data before start
+cleanFolder(UPLOAD_PATH);
+
+app.post('/profile', upload.single('avatar'), async (req, res) => {
+    try {
+        const col = await loadCollection(COLLECTION_NAME, db);
+        const data = col.insert(req.file);
+
+        db.saveDatabase();
+        res.send({ id: data.$loki, fileName: data.filename, originalName: data.originalname });
+    } catch (err) {
+        res.sendStatus(400);
+    }
+})
